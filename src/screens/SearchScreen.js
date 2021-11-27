5711b045ce937cbc672b7e71b822d1e5a1408931
manutdmohit <saudmohit@gmail.com>
2021-11-27 07:31:09 +0545

    created search bar

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const SearchScreen = () => {
+  return (
+    <View>
+      <Text>Search</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({});
+
+export default SearchScreen;

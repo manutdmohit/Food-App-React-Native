ed67189ee902f21cc406c1e0bd453e16d0818a50
manutdmohit <saudmohit@gmail.com>
2021-11-29 19:07:07 +0545

    error handling

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -6,21 +6,27 @@ import yelp from '../api/yelp';
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
   const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
 
   const searchApi = async () => {
-    const { data } = await yelp.get('/search', {
-      params: {
-        limit: 50,
-        term,
-        location: 'san jose',
-      },
-    });
-    setResults(data.businesses);
+    try {
+      const { data } = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          term,
+          location: 'san jose',
+        },
+      });
+      setResults(data.businesses);
+    } catch (error) {
+      setErrorMessage('Something went wrong');
+    }
   };
 
   return (
     <View>
       <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>Found {results.length} results</Text>
     </View>
   );

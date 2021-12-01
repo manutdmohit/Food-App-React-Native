e84f5707773842776d7ead8ae8218bdd4ce49fa3
manutdmohit <saudmohit@gmail.com>
2021-12-01 14:13:08 +0545

    hiding scroll bars

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
 import ResultList from '../components/ResultList';
@@ -21,13 +21,18 @@ const SearchScreen = () => {
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text style={{ marginLeft: 15 }}>Found {results.length} results</Text>
-      <ResultList
-        results={filterResultByPrice('$')}
-        title="Cost Effective"
-        results={filterResultByPrice('$')}
-      />
-      <ResultList results={filterResultByPrice('$$')} title="Bit Pricier" />
-      <ResultList results={filterResultByPrice('$$$')} title="Big Spender" />
+
+      <ScrollView>
+        <ResultList
+          results={filterResultByPrice('$')}
+          title="Cost Effective"
+          results={filterResultByPrice('$')}
+        />
+
+        <ResultList results={filterResultByPrice('$$')} title="Bit Pricier" />
+
+        <ResultList results={filterResultByPrice('$$$')} title="Big Spender" />
+      </ScrollView>
     </View>
   );
 };

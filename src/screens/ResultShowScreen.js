ffaee47b689609d1b491ef3cf9b3ff2f28cf759c
manutdmohit <saudmohit@gmail.com>
2021-12-01 21:33:55 +0545

    showing a list of images

--- src/screens/ResultShowScreen.js
+++ src/screens/ResultShowScreen.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Image } from 'react-native';
 import yelp from '../api/yelp';
 
 const ResultShowScreen = ({ navigation }) => {
-  const [result, setResult] = useState({});
+  const [result, setResult] = useState(null);
   const id = navigation.getParam('id');
 
   const getResult = async (id) => {
@@ -15,13 +15,27 @@ const ResultShowScreen = ({ navigation }) => {
     getResult(id);
   }, []);
 
+  if (!result) return null;
+
   return (
     <View>
       <Text>{result.name}</Text>
+      <FlatList
+        data={result.photos}
+        keyExtractor={(photo) => photo}
+        renderItem={({ item }) => (
+          <Image style={styles.image} source={{ uri: item }} />
+        )}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    height: 200,
+    width: 300,
+  },
+});
 
 export default ResultShowScreen;
